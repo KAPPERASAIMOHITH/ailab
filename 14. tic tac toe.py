9a8db2327862613378066b6a089a094c34f66989
KAPPERA SAI MOHITH <113877277+KAPPERASAIMOHITH@users.noreply.github.com>
2024-03-19 13:47:56 +0530

    Update 14. tic tac toe.py

--- 14. tic tac toe.py
+++ 14. tic tac toe.py
@@ -1,99 +1,35 @@
-
 import numpy as np
 import random
 from time import sleep
+
 def create_board():
-	return(np.array([[0, 0, 0],
-					[0, 0, 0],
-					[0, 0, 0]]))
+    return np.zeros((3, 3), dtype=int)
 
-def possibilities(board):
-	l = []
-
-	for i in range(len(board)):
-		for j in range(len(board)):
-
-			if board[i][j] == 0:
-				l.append((i, j))
-	return(l)
 def random_place(board, player):
-	selection = possibilities(board)
-	current_loc = random.choice(selection)
-	board[current_loc] = player
-	return(board)
-def row_win(board, player):
-	for x in range(len(board)):
-		win = True
+    empty_spots = [(i, j) for i in range(3) for j in range(3) if board[i][j] == 0]
+    if empty_spots:
+        i, j = random.choice(empty_spots)
+        board[i][j] = player
+    return board
 
-		for y in range(len(board)):
-			if board[x, y] != player:
-				win = False
-				continue
-
-		if win == True:
-			return(win)
-	return(win)
-def col_win(board, player):
-	for x in range(len(board)):
-		win = True
-
-		for y in range(len(board)):
-			if board[y][x] != player:
-				win = False
-				continue
-
-		if win == True:
-			return(win)
-	return(win)
-def diag_win(board, player):
-	win = True
-	y = 0
-	for x in range(len(board)):
-		if board[x, x] != player:
-			win = False
-	if win:
-		return win
-	win = True
-	if win:
-		for x in range(len(board)):
-			y = len(board) - 1 - x
-			if board[x, y] != player:
-				win = False
-	return win
-
-def evaluate(board):
-	winner = 0
-
-	for player in [1, 2]:
-		if (row_win(board, player) or
-				col_win(board, player) or
-				diag_win(board, player)):
-
-			winner = player
-
-	if np.all(board != 0) and winner == 0:
-		winner = -1
-	return winner
-
-
+def check_win(board, player):
+    return any(np.all(board == player, axis=axis) for axis in (0, 1, None)) or \
+           np.all(np.diag(board) == player) or \
+           np.all(np.diag(np.fliplr(board)) == player)
 
 def play_game():
-	board, winner, counter = create_board(), 0, 1
-	print(board)
-	sleep(2)
+    board = create_board()
+    print(board)
+    sleep(2)
 
-	while winner == 0:
-		for player in [1, 2]:
-			board = random_place(board, player)
-			print("Board after " + str(counter) + " move")
-			print(board)
-			sleep(2)
-			counter += 1
-			winner = evaluate(board)
-			if winner != 0:
-				break
-	return(winner)
+    for counter in range(1, 10):
+        for player in [1, 2]:
+            board = random_place(board, player)
+            print(f"Board after {counter} move")
+            print(board)
+            sleep(2)
+            if check_win(board, player):
+                return player
+    return -1
 
-
-# Driver Code
-print("Winner is: " + str(play_game()))
+print("Winner is:", play_game())
